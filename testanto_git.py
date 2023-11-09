617067d199074e789b7c428d0cadc7676ffe53b0
JefMD <133181706+JefMD@users.noreply.github.com>
2023-11-08 21:36:44 -0300

    Add files via upload

---
+++ testanto_git.py
@@ -0,0 +1,15 @@
+#comentario com arquivo UNMODiFILED
+
+#comentario arquivo MODIFILED 
+v = input('qual numero:')
+v1 = 1
+
+def bingo ():
+    if v == v1:
+        print('bingo')
+    else:
+        print("não foi dessa vez")
+        
+        
+        
+bingo()
